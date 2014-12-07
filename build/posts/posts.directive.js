838119adbf825b6a298bac41ae7dd51a3fa7b5a0
Peter Ingram <peter.ingram0@gmail.com>
2014-12-07 13:37:31 +0000

    dot blocks

--- build/posts/posts.directive.js
+++ build/posts/posts.directive.js
@@ -7,6 +7,12 @@
     
     dynamic.$inject = ['$compile', 'parallaxHelper'];
 
+    /**
+     * All content provided by firebase will run through this directive.
+     * The directives mission is to ensure all conent is compiled back into the DOM allowing users to bind
+     * angualr events within the HTML stored within Firebase. This is currently used for parallax
+     * @author Peter Ingram <peter.ingram0@gmail.com>
+     */
     function dynamic($compile, parallaxHelper) {
     
         var directive = {
@@ -17,6 +23,9 @@
 
         return directive;
 
+        /**
+         * Linking function
+         */
         function link(scope, ele, attrs) {
 
             scope.$watch(attrs.dynamic, function(html) {
